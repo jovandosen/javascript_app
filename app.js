162d9f0f46c1525cb378b10e5f959db2603cad43
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-06-28 16:19:08 +0200

    image slider updated

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-var x = 0;
-var timerData = 0;
-var totalImages = 0;
+var x = timerData = totalImages = 0;
 
 function slider()
 {
     var images = ['bar.jpg', 'baz.jpg', 'bird.jpg', 'dev.jpg', 'example.jpg', 'foo.jpg', 'rocks.jpg', 'test.jpg'];
-    var imageCount = images.length;
-    totalImages = images.length;
     var img = document.getElementById("app-img");
-    if(x === imageCount){
-        x = 0;
-    }
-    if(x === 0){
-        img.src = 'images/' + images[x];
-    } else {
-        img.src = 'images/' + images[x];
-    }
+    totalImages = images.length;
+    if(x === totalImages) x = 0;
+    img.src = 'images/' + images[x];
     x++;
-    var timer = setTimeout(slider, 2000);
-    timerData = timer;
+    timerData = setTimeout(slider, 2000);
 }
 
 slider();
@@ -26,15 +16,14 @@ slider();
 function stop()
 {
     clearTimeout(timerData);
-    console.log('Stopped at: ' + timerData);
     var startButton = document.getElementById("start-slider");
     startButton.style.display = 'inline-block';
 }
 
 function start()
 {
+    x = x - 1;
     slider();
-    console.log('Started at: ' + timerData);
     var stopButton = document.getElementById("start-slider");
     stopButton.style.display = 'none';
 }
@@ -43,9 +32,7 @@ function previous()
 {
     clearTimeout(timerData);
     x = x - 2;
-    if(x < 0){
-        x = totalImages - 1;
-    }
+    if(x < 0) x = totalImages - 1;
     slider();
 }
 
